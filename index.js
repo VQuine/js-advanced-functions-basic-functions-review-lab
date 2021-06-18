4cfb2d04d09990b32e4448f2f8bd2805fd519151
VQuine <01010110.v@gmail.com>
2021-06-18 19:17:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code here
+// I M P L E M E N T   FUNCTION
+function saturdayFun(firstParameter = `roller-skate`){
+    return `This Saturday, I want to ${firstParameter}!`;
+    // BACK TICKS ` ` for S T R I N G   L I T E R A L 
+    // ${E X P R E S S I O N} <- - - P L A C E H O L D E R
+};
