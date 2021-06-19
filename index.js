7b643dac801cc8b68d454f06d4d96dbc5b1c4ab0
VQuine <01010110.v@gmail.com>
2021-06-19 19:56:08 -0400

    commit tree 0a3e01c51c9bc3edfcb8b5db75d4cd2a4a4cd6c4

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-// I M P L E M E N T   FUNCTION
+                        // I M P L E M E N T   FUNCTION DECLARATION
 function saturdayFun(firstParameter = `roller-skate`){
     return `This Saturday, I want to ${firstParameter}!`;
     // BACK TICKS ` ` for S T R I N G   L I T E R A L 
     // ${E X P R E S S I O N} <- - - P L A C E H O L D E R
 };
+// H O I S T E D - can be called before declaration
+
+                        // I M P L E M E N T   FUNCTION EXPRESSION 
+/*
+function mondayWork(firstParameter = `go to the office`){
+    return `This Monday, I will ${firstParameter}.`
+}
+*/
+let mondayWork = function(firstParameter = 'go to the office'){
+    return `This Monday, I will ${firstParameter}.`
+}
+// N O T   H O I S T E D
+// MUST BE INVOKED by P O I N T E R and ( ) : 'functionExpression()'
+// 'functionExpression' without invokation - F R O Z E N
+
+                        // I M P L E M E N T    SCOPE CHAIN
+function wrapAdjective(parameter = "*"){
+//P A R E N T
+    return function(singleParameter = "special"){
+        return `You are ${parameter}${singleParameter}${parameter}!`
+    }
+    // I N N E R   F U N C T I O N
+}
+
+const Calculator = {}
